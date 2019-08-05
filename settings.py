eae32ddd90d59244794c7cbcbfcd60b7279c9304
Sergey <immortalizedinhearts@gmail.com>
2019-08-05 18:39:04 +0300

    Created settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Stores all game settings"""
+
+    def __init__(self):
+        """Initializes game settings"""
+        # Screen parameters
+        self.screen_width = 1200
+        self.screen_hight = 800
+        self.bg_color = (230, 230, 230)
