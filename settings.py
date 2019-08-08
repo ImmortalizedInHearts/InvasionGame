c3b8b9a52988e5898419750fa659ec9df2a59f1c
Sergey <immortalizedinhearts@gmail.com>
2019-08-08 21:46:16 +0300

    Added ship speed regulation and ship parameters

--- settings.py
+++ settings.py
@@ -7,3 +7,6 @@ class Settings():
         self.screen_width = 1200
         self.screen_hight = 800
         self.bg_color = (230, 230, 230)
+
+        # Ship parameters
+        self.ship_speed_factor = 1.5
--- ship.py
+++ ship.py
@@ -4,9 +4,10 @@ import pygame
 class Ship():
     """Describes ship behavior"""
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initializes ship and it's first position"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Loading ship image and getting a rectangle
         self.image = pygame.image.load('images/ship.bmp')
@@ -16,16 +17,22 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # Determination ship center coordinates(float)
+        self.center = float(self.rect.centerx)
         # Moving flags
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Updating ship position due to flags"""
+        # Updates center attribute, not rect
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.rect.centerx -= self.ai_settings.ship_speed_factor
+
+        # Updating rect attribute on the base of self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draws ship in current position"""
