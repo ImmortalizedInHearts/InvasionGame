3f45cbf5910ce740427a10f43cdbdeb91ec9622f
Sergey <immortalizedinhearts@gmail.com>
2019-08-09 00:05:02 +0300

    Added shooting by pressing SPACE

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -16,11 +17,15 @@ def run_game():
     # Ship creation
     ship = Ship(ai_settings, screen)
 
+    # Creating a group for storing bullets
+    bullets = Group()
+
     # Launch of the main game loop
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -2,16 +2,21 @@ import sys
 
 import pygame
 
+from bullet import Bullet
+
 
 """Stores functions provide game process"""
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Reacts on key pushing"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -22,21 +27,24 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     # Tracking keyboard and mouse events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship):
-    """Update images on screeen and displays new screen"""
+def update_screen(ai_settings, screen, ship, bullets):
+    """Update images on screen and displays new screen"""
     # Each time loop pass, screen is redrawn
     screen.fill(ai_settings.bg_color)
+    # All bullets displays behind images of ship and aliens
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
     # Displaing last traced screen
     pygame.display.flip()
